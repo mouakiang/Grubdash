520e15d55932c9562cc25413755f5b1679aecb79
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-07-12 21:17:15 -0400

    dishes controller completed

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -8,10 +8,10 @@ const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
 
-function dishExists(req, res, next){
-  const {dishId} = req.params;
-  const foundDish = dishes.find((dish) => dish.id === (dishId));
-  if (foundDish){
+function dishExists(req, res, next) {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+  if (foundDish) {
     res.locals.dish = foundDish;
     return next();
   }
@@ -22,32 +22,11 @@ function dishExists(req, res, next){
 }
 
 function read(req, res, next) {
- res.json({data: res.locals.dish});
-  }
-
+  res.json({ data: res.locals.dish });
+}
 
 function create(req, res, next) {
-  let message = "";
   const { data: { name, description, price, image_url } = {} } = req.body;
-
-  if (!name || name === "") {
-    message = "Dish must include a name";
-  } else if (!description || description === "") {
-    message = "Dish must include a description";
-  } else if (!price) {
-    message = "Dish must include a price";
-  } else if (price <= 0 || !Number.isInteger(price)) {
-    message = "Dish must have a price that is an integer greater than 0";
-  } else if (!image_url || image_url === "") {
-    message = "Dish must include an image_url";
-  }
-
-  if (message) {
-    return next({
-      status: 400,
-      message: message,
-    });
-  }
 
   const newDish = {
     id: nextId(),
@@ -61,12 +40,80 @@ function create(req, res, next) {
   res.status(201).json({ data: newDish });
 }
 
-function list(req, res){
-  res.json({data: dishes});
+function update(req, res) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  res.locals.dish = {
+    id: res.locals.dish.id,
+    name,
+    description,
+    price,
+    image_url,
+  };
+  res.json({ data: res.locals.dish });
+}
+
+
+
+function list(req, res) {
+  res.json({ data: dishes });
+}
+
+function hasName(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({ status: 400, message: `Must include a ${propertyName}` });
+  };
+}
+
+function pricePropertyIsValid(req, res, next) {
+  const { data: { price } = {} } = req.body;
+  if (!Number.isInteger(price) || price <= 0) {
+    return next({
+      status: 400,
+      message: "Dish must have a price that is an integer greater than 0",
+    });
+  }
+  return next();
+}
+
+function dishIdCheck(req, res, next) {
+  const {data: {id} = {}} = req.body;
+  const {dishId} = req.params;
+  if(!dishId) {
+    return next({
+      status: 400, 
+      message: `Dish does not exist: ${dishId}.`
+    });
+  } else if (id && id !== dishId){
+    return next({
+      status: 400,
+      message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`
+    });
+  }
+  return next();
 }
 
 module.exports = {
-  create,
-  read: [dishExists, read],
+   create: [
+      hasName("name"),
+      hasName("description"),         hasName("price"), 
+      hasName("image_url"),  
+      pricePropertyIsValid, 
+      create
+    ],
   list,
+  read: [dishExists, read],
+  update: [
+      dishExists,
+      hasName("name"),
+      hasName("description"),
+      hasName("price"),
+      hasName("image_url"),
+      pricePropertyIsValid,
+      dishIdCheck,
+      update
+  ]
 };

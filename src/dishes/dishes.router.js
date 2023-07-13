e040b4be537ddf3302f96ace5da4488f7b59c143
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-07-12 21:18:06 -0400

    dishes router completed

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -3,12 +3,13 @@ const controller = require("./dishes.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 // TODO: Implement the /dishes routes needed to make the tests pass
 router.route("/")
-  .post(controller.create)
   .get(controller.list)
+  .post(controller.create)
   .all(methodNotAllowed)
 
 router.route("/:dishId")
 .get(controller.read)
+.put(controller.update)
 .all(methodNotAllowed)
 
 

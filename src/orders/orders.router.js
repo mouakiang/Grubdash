e57d60553ea6e17b93cecb97ba131a0e4cabe172
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-07-12 21:29:40 -0400

    added read router to orders router

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,6 +1,12 @@
 const router = require("express").Router();
-
+const controller = require("./orders.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 // TODO: Implement the /orders routes needed to make the tests pass
+router.route("/")
+
+router.route("/:orderId")
+  .get(controller.read)
+
 
 module.exports = router;
 
